b29fad71d9798e606baa5d3d6788502c2c18d617
Oleg Gushul <gushul.o@gmail.com>
2018-02-24 14:55:50 +0300

    STEP: 19

--- make-app.mk
+++ make-app.mk
@@ -9,8 +9,7 @@ app-build:
 app-bash:
 	docker-compose run --user=$(USER) app bash
 
-app-setup:
-	docker-compose build
+app-setup: development-setup-env app-build
 	docker-compose run --user=$(USER) app bin/setup
 
 
